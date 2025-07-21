5c5a15899a93020c75a51d4de4ef83a04a9e5e24
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-21 19:27:05 -0300

    feat: adds first valid test to sign-up-user.spec file

--- domain/src/use-cases/sign-up-user.spec.ts
+++ domain/src/use-cases/sign-up-user.spec.ts
@@ -2,11 +2,7 @@ import { describe, it, expect, test } from "vitest";
 import { signUpUser } from "./sign-up-user";
 
 describe("UserRegister Use Case", async () => {
-  test("With an email already in use, fails with InvalidData", async () => {
-    const payload = {
-      email: "existing@user.com",
-      password: "12345678",
-      username: "Test User",
-    };
+  it("should throw an error if input is invalid", () => {
+    expect(() => signUpUser()).toThrow("Not implemented");
   });
 });
