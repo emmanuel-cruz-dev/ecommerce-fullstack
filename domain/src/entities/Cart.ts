626719b86b6ea5e2e76bb553611a6d03f277cbcc
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-22 12:48:43 -0300

    feat: adds CartProps and ProductProps to it in Cart entity

--- domain/src/entities/Cart.ts
+++ domain/src/entities/Cart.ts
@@ -1,6 +1,12 @@
+import { ProductProps } from "./Product";
+
+export interface CartProps {
+  id: string;
+  userId: string;
+  items: ProductProps[];
+}
+
 export interface CartItemProps {
   productId: string;
-  productName: string;
-  productPrice: string;
   quantity: number;
 }
