34e2fc422ecb20725c15ad9478aff8c553bc3e99
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-22 16:44:16 -0300

    refactor: adds validateFields function to handle validations in CreateProduct function

--- domain/src/use-cases/create-product.ts
+++ domain/src/use-cases/create-product.ts
@@ -1,19 +1,23 @@
 import { ProductProps } from "../entities/Product";
 import { ProductRepository } from "../repositories/product-repository";
 
+function validateFields(name: string, price: number, stock: number): void {
+  if (!name || name.trim() === "") {
+    throw new Error("Name is required");
+  }
+  if (price <= 0) {
+    throw new Error("Price must be greater than 0");
+  }
+  if (stock < 0) {
+    throw new Error("Stock cannot be 0 or negative");
+  }
+}
+
 export function CreateProduct(
   productData: ProductProps,
   repository: ProductRepository
 ): ProductProps {
-  if (!productData.name || productData.name.trim() === "") {
-    throw new Error("Name is required");
-  }
-  if (productData.price <= 0) {
-    throw new Error("Price must be greater than 0");
-  }
-  if (productData.stock < 0) {
-    throw new Error("Stock cannot be 0 or negative");
-  }
+  validateFields(productData.name, productData.price, productData.stock);
 
   repository.save(productData);
 
