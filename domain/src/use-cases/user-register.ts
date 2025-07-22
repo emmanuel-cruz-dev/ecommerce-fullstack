7ebf4ea9ade050db8edb1db9c9751a66148e9d5a
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-22 11:11:27 -0300

    feat: adds validateData function to user-register file in use-cases folder

--- domain/src/use-cases/user-register.ts
+++ domain/src/use-cases/user-register.ts
@@ -1,3 +1,19 @@
-export function UserRegister() {
-  throw new Error("Not implemented");
+import { UserProps } from "../entities/User";
+
+function validateData(email: string, password: string, username: string): void {
+  if (email.trim() === "") {
+    throw new Error("Email is required");
+  }
+  if (password.trim() === "") {
+    throw new Error("Password is required");
+  }
+  if (username.trim() === "") {
+    throw new Error("Username is required");
+  }
+}
+
+export function UserRegister(user: UserProps): UserProps {
+  validateData(user.email, user.password, user.username);
+
+  return user;
 }
