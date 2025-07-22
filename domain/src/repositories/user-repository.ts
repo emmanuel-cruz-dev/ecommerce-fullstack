fe8a012f3cba98b814092f46716226cb630d5a17
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-22 09:33:36 -0300

    feat: adds user-repository file to repositories folder

---
+++ domain/src/repositories/user-repository.ts
@@ -0,0 +1,6 @@
+import { UserProps } from "../entities/User";
+
+export interface UserRepository {
+  findByEmail(email: string): Promise<UserProps | null>;
+  save(user: UserProps): Promise<void>;
+}
