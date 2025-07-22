b3af9a9fa010a6b9654e0d76092cb60f71a2dc8f
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-22 16:36:43 -0300

    feat: adds validations to CreateProduct function in create-product file

--- domain/src/use-cases/create-product.ts
+++ domain/src/use-cases/create-product.ts
@@ -5,6 +5,17 @@ export function CreateProduct(
   productData: ProductProps,
   repository: ProductRepository
 ): ProductProps {
+  if (!productData.name || productData.name.trim() === "") {
+    throw new Error("Name is required");
+  }
+  if (productData.price <= 0) {
+    throw new Error("Price must be greater than 0");
+  }
+  if (productData.stock < 0) {
+    throw new Error("Stock cannot be 0 or negative");
+  }
+
   repository.save(productData);
+
   return productData;
 }
