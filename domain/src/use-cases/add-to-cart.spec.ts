1bf6697e013a18b109726e0f7daa572fd8a681f7
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-22 14:49:18 -0300

    feat: adds beforeEach and first test to add-to-cart.spec file

--- domain/src/use-cases/add-to-cart.spec.ts
+++ domain/src/use-cases/add-to-cart.spec.ts
@@ -1,5 +1,35 @@
-import { describe, it } from "vitest";
+import { beforeEach, describe, expect, it } from "vitest";
+import { AddToCart, AddToCartRequest } from "./add-to-cart";
+import {
+  mockCartRepository,
+  MockedCartRepository,
+} from "../mocks/cart-repository-mock";
 
 describe("AddToCart Use Case", () => {
-  it("should create a new cart when user has no existing cart", () => {});
+  let validRequest: AddToCartRequest;
+  let mockRepository: MockedCartRepository;
+
+  beforeEach(() => {
+    validRequest = {
+      userId: "user-1",
+      productId: "product-1",
+      productName: "Test Product",
+      productPrice: "19.99",
+      quantity: 2,
+    };
+
+    mockRepository = mockCartRepository();
+  });
+
+  it("should create a new cart when user has no existing cart", async () => {
+    const cart = await AddToCart(validRequest, mockRepository);
+
+    expect(cart.userId).toBe(validRequest.userId);
+    expect(cart.items).toHaveLength(1);
+    expect(cart.items[0]).toEqual({
+      productId: validRequest.productId,
+      quantity: validRequest.quantity,
+    });
+    expect(mockRepository.carts).toHaveLength(1);
+  });
 });
