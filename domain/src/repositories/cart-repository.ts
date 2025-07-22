9476d42786e23b9825581865fdf4942046d3462e
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-22 14:50:31 -0300

    refactor: updates CartRepository interface in repositories folder

--- domain/src/repositories/cart-repository.ts
+++ domain/src/repositories/cart-repository.ts
@@ -1,8 +1,7 @@
 import { CartProps } from "../entities/Cart";
 
 export interface CartRepository {
-  findById(cartId: string): Promise<CartProps | null>;
-  findByUserId(userId: string): Promise<CartProps | null>;
-  save(cart: CartProps): Promise<CartProps>;
-  delete(cartId: string): Promise<void>;
+  carts: CartProps[];
+  findCartByUserId(userId: string): CartProps | undefined;
+  saveCart(cart: CartProps): void;
 }
