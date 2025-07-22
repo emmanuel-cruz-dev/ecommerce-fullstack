d633d94cd32ddf92bd8cc4dde141f8a7eb0e6504
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-22 16:17:15 -0300

    feat: adds 'create product successfully' test to create-product.spec file in use-cases folder

--- domain/src/use-cases/create-product.spec.ts
+++ domain/src/use-cases/create-product.spec.ts
@@ -1,9 +1,31 @@
-import { describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it } from "vitest";
+import { ProductRepository } from "../repositories/product-repository";
+import { mockProductRepository } from "../mocks/product-repository";
 import { CreateProduct } from "./create-product";
+import { ProductProps } from "../entities/Product";
 
 describe("CreateProduct Use Case", () => {
-  it("should return null", () => {
-    const product = CreateProduct();
-    expect(product).toEqual(null);
+  let mockRepository: ProductRepository;
+  let validProduct: ProductProps;
+
+  beforeEach(() => {
+    mockRepository = mockProductRepository();
+  });
+
+  it("should create a product successfully", () => {
+    validProduct = {
+      id: "1",
+      name: "Remera",
+      description: "Remera de algodón",
+      price: 200,
+      stock: 20,
+      category: "Ropa",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    const product = CreateProduct(validProduct, mockRepository);
+
+    expect(product).toEqual(product);
+    expect(mockRepository.findById(product.id)).toEqual(product);
   });
 });
