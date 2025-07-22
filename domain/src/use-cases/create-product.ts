6f37fe1c28bd95f7f438816442cb5e30a2d38d01
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-22 16:16:23 -0300

    feat: adds first structure to CreateProduct function in use-cases folder

--- domain/src/use-cases/create-product.ts
+++ domain/src/use-cases/create-product.ts
@@ -1,3 +1,10 @@
-export function CreateProduct() {
-  return null;
+import { ProductProps } from "../entities/Product";
+import { ProductRepository } from "../repositories/product-repository";
+
+export function CreateProduct(
+  productData: ProductProps,
+  repository: ProductRepository
+): ProductProps {
+  repository.save(productData);
+  return productData;
 }
