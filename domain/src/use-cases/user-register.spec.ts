092aaaf99555311f34c71c0b17dda02251181d64
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-22 09:27:04 -0300

    refactor: renames user-register files in use-cases

--- domain/src/use-cases/user-register.spec.ts
+++ domain/src/use-cases/user-register.spec.ts
@@ -1,8 +1,8 @@
 import { describe, it, expect, test } from "vitest";
-import { signUpUser } from "./sign-up-user";
+import { UserRegister } from "./user-register";
 
 describe("UserRegister Use Case", async () => {
   it("should throw an error if input is invalid", () => {
-    expect(() => signUpUser()).toThrow("Not implemented");
+    expect(() => UserRegister()).toThrow("Not implemented");
   });
 });
--- domain/src/use-cases/user-register.ts
+++ domain/src/use-cases/user-register.ts
@@ -1,3 +1,3 @@
-export function signUpUser() {
+export function UserRegister() {
   throw new Error("Not implemented");
 }
