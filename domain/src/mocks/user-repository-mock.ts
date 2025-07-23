c29a321a6f08dd10b783f91ef9d5be64983a7f84
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-23 07:43:14 -0300

    refactor: renames UserProps to User in user-repository-mocks file

--- domain/src/mocks/user-repository-mock.ts
+++ domain/src/mocks/user-repository-mock.ts
@@ -1,21 +1,19 @@
-import { UserProps } from "../entities/User";
+import { User } from "../entities/User";
 import { UserRepository } from "../repositories/user-repository";
 
 export interface MockedUserRepository extends UserRepository {
-  users: UserProps[];
+  users: User[];
 }
 
-export function mockUserRepository(
-  users: UserProps[] = []
-): MockedUserRepository {
+export function mockUserRepository(users: User[] = []): MockedUserRepository {
   return {
     users,
-    findByEmail: async (email: string): Promise<UserProps | null> => {
+    findByEmail: async (email: string): Promise<User | null> => {
       const user = users.find((user) => user.email === email);
       const result = user ? { ...user } : null;
       return result;
     },
-    save: async (user: UserProps): Promise<UserProps> => {
+    save: async (user: User): Promise<User> => {
       const existingUserIndex = users.findIndex((u) => u.email === user.email);
       if (existingUserIndex !== -1) {
         users[existingUserIndex] = { ...user };
