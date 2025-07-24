6003c89e726f4b39c7d3640b7222110b5d999d37
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-24 16:51:59 -0300

    feat: adds description validation in validateFields, create-product file

--- domain/src/use-cases/create-product.ts
+++ domain/src/use-cases/create-product.ts
@@ -5,16 +5,29 @@ export function CreateProduct(
   productData: Product,
   repository: ProductRepository
 ): Product {
-  validateFields(productData.name, productData.price, productData.stock);
+  validateFields(
+    productData.name,
+    productData.description,
+    productData.price,
+    productData.stock
+  );
 
   repository.save(productData);
 
   return productData;
 }
 
-function validateFields(name: string, price: number, stock: number): void {
+function validateFields(
+  name: string,
+  description: string,
+  price: number,
+  stock: number
+): void {
   if (!name || name.trim() === "") {
     throw new Error("Name is required");
+  }
+  if (!description || description.trim() === "") {
+    throw new Error("Description is required");
   }
   if (price <= 0) {
     throw new Error("Price must be greater than 0");
