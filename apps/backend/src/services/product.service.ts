b7efeaff928a23d6317b1d37911442646cce35d5
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-29 16:35:41 -0300

    feat: adds product.services file and its functions to handle get, create, update and delete products

---
+++ apps/backend/src/services/product.service.ts
@@ -0,0 +1,13 @@
+const getAllProducts = async () => {};
+const getProductById = async () => {};
+const createProduct = async () => {};
+const updateProduct = async () => {};
+const deleteProduct = async () => {};
+
+export default {
+  getAllProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+};
