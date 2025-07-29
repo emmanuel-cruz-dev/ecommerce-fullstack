a742f74b13d4fed647306bf9ef071b80c4dc3af1
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-29 16:55:04 -0300

    feat: adds functionality to getProductById function in product.controller file

--- apps/backend/src/controllers/product.controller.ts
+++ apps/backend/src/controllers/product.controller.ts
@@ -17,7 +17,30 @@ const getAllProducts = async (req: Request, res: Response) => {
   }
 };
 
-const getProductById = (req: Request, res: Response) => {};
+const getProductById = async (req: Request, res: Response) => {
+  const id = req.params.productId;
+
+  try {
+    const product = await productService.getProductById(id);
+
+    if (!product) {
+      return res
+        .status(404)
+        .json({ ok: false, message: `Producto con ID '${id}' no encontrado` });
+    }
+
+    res.status(200).json({ ok: true, payload: product });
+  } catch (error: unknown) {
+    if (error instanceof HttpError) {
+      res.status(error.status).json({ ok: false, message: error.message });
+    } else {
+      res
+        .status(500)
+        .json({ ok: false, message: "Error interno del servidor" });
+    }
+  }
+};
+
 const createProduct = (req: Request, res: Response) => {};
 const updateProduct = (req: Request, res: Response) => {};
 const deleteProduct = (req: Request, res: Response) => {};
