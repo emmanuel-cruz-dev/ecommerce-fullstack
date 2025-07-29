39eace37c7bc5ce319a022d9e355e5754807e5f1
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-29 17:29:33 -0300

    feat: adds handleError function to error.ts file in errors folder

--- apps/backend/src/errors/error.ts
+++ apps/backend/src/errors/error.ts
@@ -1,3 +1,5 @@
+import { Response } from "express";
+
 export class HttpError extends Error {
   status: number;
   constructor(message: string, status = 500) {
@@ -5,3 +7,17 @@ export class HttpError extends Error {
     this.status = status;
   }
 }
+
+export const handleError = (res: Response, error: unknown) => {
+  if (error instanceof HttpError) {
+    return res.status(error.status).json({
+      ok: false,
+      message: error.message,
+    });
+  }
+
+  return res.status(500).json({
+    ok: false,
+    message: "Error interno del servidor",
+  });
+};
