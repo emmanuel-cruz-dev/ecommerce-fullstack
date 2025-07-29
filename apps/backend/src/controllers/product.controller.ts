12805767f2674f13f8207d6f0d8efeeb87274086
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-29 16:37:37 -0300

    feat: adds functionality to getAllProducts function in product.controller file

--- apps/backend/src/controllers/product.controller.ts
+++ apps/backend/src/controllers/product.controller.ts
@@ -1,6 +1,22 @@
 import { Request, Response } from "express";
+import { HttpError } from "src/errors/error";
+import productService from "src/services/product.service";
 
-const getAllProducts = (req: Request, res: Response) => {};
+const getAllProducts = async (req: Request, res: Response) => {
+  try {
+    const products = await productService.getAllProducts();
+    res.status(200).json({ ok: true, payload: products });
+  } catch (error: unknown) {
+    if (error instanceof HttpError) {
+      res.status(error.status).json({ ok: false, message: error.message });
+    } else {
+      res
+        .status(500)
+        .json({ ok: false, message: "Error interno del servidor" });
+    }
+  }
+};
+
 const getProductById = (req: Request, res: Response) => {};
 const createProduct = (req: Request, res: Response) => {};
 const updateProduct = (req: Request, res: Response) => {};
