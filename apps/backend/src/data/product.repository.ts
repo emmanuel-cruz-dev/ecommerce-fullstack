b9f433659ef2618775813d856ae0db98f63a4f8e
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-30 19:43:55 -0300

    feat: adds save function to product.repository and updates productsDB imports

--- apps/backend/src/data/product.repository.ts
+++ apps/backend/src/data/product.repository.ts
@@ -1,14 +1,29 @@
-import productDB from "../database/products.db.json";
+import { Product } from "@domain/src/entities/Product";
+import { productsDB } from "../database/products.db";
 
 const getAllProducts = async () => {
-  return productDB;
+  return productsDB;
 };
 
 const findById = async (id: string) => {
-  return productDB.find((product) => product.id === id) || null;
+  return productsDB.find((product) => product.id === id) || null;
+};
+
+const save = async (product: Product) => {
+  const existingIndex = productsDB.findIndex((prod) => prod.id === product.id);
+
+  if (existingIndex >= 0) {
+    productsDB[existingIndex] = product;
+  } else {
+    const newProduct = { ...product, id: "prod-004" };
+    productsDB.push(newProduct);
+    return newProduct;
+  }
+  return product;
 };
 
 export default {
   getAllProducts,
   findById,
+  save,
 };
