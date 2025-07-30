32b243210fdc4077aa578733f1fbf53461df97b0
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-30 16:36:36 -0300

    feat: imports productRepository in product.service file and comments updateProduct function

--- apps/backend/src/services/product.service.ts
+++ apps/backend/src/services/product.service.ts
@@ -1,16 +1,27 @@
-const getAllProducts = async () => {};
+import productRepository from "../data/product.repository";
+
+const getAllProducts = async () => {
+  return await productRepository.getAllProducts();
+};
+
 const getProductById = async (productId: string) => {
   return productId;
 };
 
-const createProduct = async () => {};
-const updateProduct = async () => {};
-const deleteProduct = async () => {};
+const createProduct = async (product: string) => {};
+
+// const updateProduct = async (productId: string, body) => {
+//   return body;
+// };
+
+const deleteProduct = async (productId: string) => {
+  return productId;
+};
 
 export default {
   getAllProducts,
   getProductById,
   createProduct,
-  updateProduct,
+  //updateProduct,
   deleteProduct,
 };
