ede93729ad288026d56cfd889a483ed408a2243f
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-31 15:52:40 -0300

    feat: adds authenticate and authorize middlewares to product.routes to divide routes publics and protected

--- apps/backend/src/routes/product.routes.ts
+++ apps/backend/src/routes/product.routes.ts
@@ -1,13 +1,31 @@
 import { Router } from "express";
 import productController from "../controllers/product.controller";
+import { authenticate, authorize } from "../middlewares/auth.middleware";
 
 const router = Router();
 
-router
-  .get("/", productController.getAllProducts)
-  .get("/:productId", productController.getProductById)
-  .post("/", productController.createProduct)
-  .put("/:productId", productController.updateProduct)
-  .delete("/:productId", productController.deleteProduct);
+// Public routes
+router.get("/", productController.getAllProducts);
+router.get("/:productId", productController.getProductById);
+
+// Protected routes
+router.post(
+  "/",
+  authenticate,
+  authorize(["admin"]),
+  productController.createProduct
+);
+router.put(
+  "/:productId",
+  authenticate,
+  authorize(["admin"]),
+  productController.updateProduct
+);
+router.delete(
+  "/:productId",
+  authenticate,
+  authorize(["admin"]),
+  productController.deleteProduct
+);
 
 export default router;
