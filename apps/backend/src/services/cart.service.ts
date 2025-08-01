5b1e6645a084672ad226272ef6dd0998ed6597af
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-08-01 15:14:22 -0300

    feat: adds cart.service file to backend/src/services folder

---
+++ apps/backend/src/services/cart.service.ts
@@ -0,0 +1,57 @@
+import { CartItem } from "@domain/src/entities/Cart";
+import cartRepository from "../data/cart.repository";
+import { v4 as uuid } from "uuid";
+
+interface AddToCartRequest {
+  userId: string;
+  productId: string;
+  productName: string;
+  productPrice: number;
+  quantity: number;
+}
+
+const getCartContent = async (userId: string) => {
+  return await cartRepository.findCartByUserId(userId);
+};
+
+const addToCart = async (request: AddToCartRequest) => {
+  const { userId, productId, quantity } = request;
+  let cart = await cartRepository.findCartByUserId(userId);
+
+  if (!cart) {
+    cart = {
+      id: uuid(),
+      userId: userId,
+      items: [],
+    };
+  }
+
+  const existingItem = cart.items.find((item) => item.productId === productId);
+
+  if (existingItem) {
+    existingItem.quantity += quantity;
+  } else {
+    const newItem: CartItem = {
+      productId: productId,
+      quantity: quantity,
+    };
+    cart.items.push(newItem);
+  }
+
+  return await cartRepository.saveCart(cart);
+};
+
+const clearCart = async (userId: string) => {
+  return await cartRepository.clearCart(userId);
+};
+
+const removeFromCart = async (userId: string, productId: string) => {
+  return await cartRepository.removeCartItem(userId, productId);
+};
+
+export default {
+  getCartContent,
+  addToCart,
+  clearCart,
+  removeFromCart,
+};
