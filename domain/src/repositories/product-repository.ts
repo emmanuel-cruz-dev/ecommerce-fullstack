2fbce6a6e0d70ee0a11b08fd09ee4682b5ac7a89
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-08-01 12:11:59 -0300

    feat: adds findAll and delete functionalities to interface ProductRepository

--- domain/src/repositories/product-repository.ts
+++ domain/src/repositories/product-repository.ts
@@ -3,4 +3,6 @@ import { Product } from "../entities/Product";
 export interface ProductRepository {
   save(product: Product): void;
   findById(id: string): Product | undefined;
+  findAll(): Product[];
+  delete(id: string): boolean;
 }
