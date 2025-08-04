42c4be3c3e53fff3160f3c815c1ddcef57ff6f41
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-08-04 12:01:21 -0300

    feat: adds types folder and file to handle interfaces and types project

---
+++ apps/backend/src/types/types.ts
@@ -0,0 +1,25 @@
+import { UserRole } from "@domain/src/entities/User";
+import { Request } from "express";
+
+export interface AuthPayload {
+  id: string;
+  email: string;
+  role: UserRole;
+}
+
+export interface AuthenticatedRequest extends Request {
+  user?: {
+    id: string;
+    email: string;
+    username: string;
+    role: string;
+  };
+}
+
+export interface AddToCartRequest {
+  userId: string;
+  productId: string;
+  productName: string;
+  productPrice: number;
+  quantity: number;
+}
