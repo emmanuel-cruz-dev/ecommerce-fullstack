c71784f11a5d979512800b577df3765d34459a74
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-08-04 12:10:31 -0300

    refactor: updates functions structure in cart.controller

--- apps/backend/src/controllers/cart.controller.ts
+++ apps/backend/src/controllers/cart.controller.ts
@@ -2,87 +2,93 @@ import { Request, Response } from "express";
 import { AuthenticatedRequest } from "@domain/src/ports/auth-types";
 import cartService from "../services/cart.service";
 
-export default {
-  async addToCart(req: Request, res: Response): Promise<Response> {
-    const { productId, productName, productPrice, quantity } = req.body;
-    const userId = (req as AuthenticatedRequest).user.id;
+const addToCart = async (req: Request, res: Response): Promise<Response> => {
+  const { productId, productName, productPrice, quantity } = req.body;
+  const userId = (req as AuthenticatedRequest).user.id;
 
-    try {
-      const request = {
-        userId,
-        productId,
-        productName,
-        productPrice,
-        quantity,
-      };
-      const updatedCart = await cartService.addToCart(request);
+  try {
+    const request = {
+      userId,
+      productId,
+      productName,
+      productPrice,
+      quantity,
+    };
+    const updatedCart = await cartService.addToCart(request);
 
-      return res.status(200).json({ ok: true, payload: updatedCart });
-    } catch (error) {
-      return res
-        .status(400)
-        .json({ ok: false, message: (error as Error).message });
-    }
-  },
-
-  async getCartContent(req: Request, res: Response): Promise<Response> {
-    const userId = (req as AuthenticatedRequest).user.id;
-
-    try {
-      const cart = await cartService.getCartContent(userId);
-
-      return res.status(200).json({ ok: true, payload: cart });
-    } catch (error) {
-      return res
-        .status(500)
-        .json({ ok: false, message: "Error interno del servidor" });
-    }
-  },
-
-  async removeFromCart(req: Request, res: Response): Promise<Response> {
-    const { productId } = req.params;
-    const userId = (req as AuthenticatedRequest).user.id;
-
-    try {
-      const success = await cartService.removeFromCart(userId, productId);
-
-      if (!success) {
-        return res.status(404).json({
-          ok: false,
-          message: "El producto no se encontró en el carrito",
-        });
-      }
-
-      return res
-        .status(200)
-        .json({ ok: true, message: "Producto eliminado del carrito" });
-    } catch (error) {
-      return res
-        .status(500)
-        .json({ ok: false, message: "Error interno del servidor" });
-    }
-  },
-
-  async clearCart(req: Request, res: Response): Promise<Response> {
-    const userId = (req as AuthenticatedRequest).user.id;
-
-    try {
-      const success = await cartService.clearCart(userId);
-
-      if (!success) {
-        return res.status(404).json({
-          ok: false,
-          message: "El carrito del usuario no se encontró",
-        });
-      }
-
-      return res
-        .status(200)
-        .json({ ok: true, message: "Carrito vaciado correctamente" });
-    } catch (error) {
-      return res
-        .status(500)
-        .json({ ok: false, message: "Error interno del servidor" });
-    }
-  },
+    return res.status(200).json({ ok: true, payload: updatedCart });
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ ok: false, message: (error as Error).message });
+  }
 };
+
+const getCartContent = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const userId = (req as AuthenticatedRequest).user.id;
+
+  try {
+    const cart = await cartService.getCartContent(userId);
+
+    return res.status(200).json({ ok: true, payload: cart });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ ok: false, message: "Error interno del servidor" });
+  }
+};
+
+const removeFromCart = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const { productId } = req.params;
+  const userId = (req as AuthenticatedRequest).user.id;
+
+  try {
+    const success = await cartService.removeFromCart(userId, productId);
+
+    if (!success) {
+      return res.status(404).json({
+        ok: false,
+        message: "El producto no se encontró en el carrito",
+      });
+    }
+
+    return res
+      .status(200)
+      .json({ ok: true, message: "Producto eliminado del carrito" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ ok: false, message: "Error interno del servidor" });
+  }
+};
+
+const clearCart = async (req: Request, res: Response): Promise<Response> => {
+  const userId = (req as AuthenticatedRequest).user.id;
+
+  try {
+    const success = await cartService.clearCart(userId);
+
+    if (!success) {
+      return res.status(404).json({
+        ok: false,
+        message: "El carrito del usuario no se encontró",
+      });
+    }
+
+    return res
+      .status(200)
+      .json({ ok: true, message: "Carrito vaciado correctamente" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ ok: false, message: "Error interno del servidor" });
+  }
+};
+
+export default { addToCart, getCartContent, removeFromCart, clearCart };
