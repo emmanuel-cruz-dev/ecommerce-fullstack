44d6b52343e4686baba179b3f81d8117cb2aa0de
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-08-06 19:47:19 -0300

    feat: adds ProductListPage to App.tsx file to show products list and container

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,16 +1,7 @@
+import { ProductListPage } from "./pages/ProductListPage";
+
 function App() {
-  return (
-    <div className="m-12">
-      <div className="bg-white p-8 rounded-lg shadow-lg">
-        <h1 className="text-3xl font-bold text-gray-800 mb-4">
-          Prueba de Tailwind
-        </h1>
-        <p className="text-gray-600 mb-4">
-          Tailwind funcionando correctamente.
-        </p>
-      </div>
-    </div>
-  );
+  return <ProductListPage />;
 }
 
 export default App;
