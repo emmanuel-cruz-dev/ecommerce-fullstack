3018f2e4c5b29e2928b995ff029309142043dae3
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-08-06 20:24:56 -0300

    feat: adds Link to ProductCard to navigate to products details

--- apps/frontend/src/components/ui/ProductCard.tsx
+++ apps/frontend/src/components/ui/ProductCard.tsx
@@ -1,13 +1,13 @@
 import { type FC } from "react";
-import type { Product } from "@domain/entities/Product";
-
-interface ProductCardProps {
-  product: Product;
-}
+import { Link } from "react-router-dom";
+import type { ProductCardProps } from "src/types/product";
 
 export const ProductCard: FC<ProductCardProps> = ({ product }) => {
   return (
-    <article className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-xl transition-shadow duration-300 max-w-sm">
+    <Link
+      to={`/products/${product.id}`}
+      className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-xl transition-shadow duration-300 max-w-sm"
+    >
       <figure className="w-32 h-40 mx-auto">
         <img
           className="w-full h-full object-cover"
@@ -19,10 +19,15 @@ export const ProductCard: FC<ProductCardProps> = ({ product }) => {
         <h3 className="text-lg font-semibold text-gray-800">{product.name}</h3>
         <p className="mt-1 text-gray-600">{product.description}</p>
         <p className="mt-2 text-xl font-bold text-gray-900">${product.price}</p>
-        <button className="mt-4 w-full bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-600 transition-colors">
+        <button
+          className="mt-4 w-full bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-600 transition-colors"
+          onClick={(e) => {
+            e.preventDefault();
+          }}
+        >
           Añadir al carrito
         </button>
       </div>
-    </article>
+    </Link>
   );
 };
