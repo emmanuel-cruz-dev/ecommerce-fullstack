63125caa4594612e0517dbced40a2f2712f3a9c5
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-08-06 15:40:22 -0300

    refactor: updates App.tsx file content

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,48 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p className="min-h-screen bg-blue-300 font-bold">Hola</p>
-      <div className="min-h-screen">
-        <h1 className="bg-red-300 font-bold">Vite - React</h1>
-        <div className="bg-white p-8 rounded-lg shadow-lg">
-          <h1 className="text-3xl font-bold text-gray-800 mb-4">
-            Prueba de Tailwind
-          </h1>
-          <p className="text-gray-600 mb-4">
-            Si ves este texto estilizado, Tailwind funciona correctamente.
-          </p>
-          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-            Botón de prueba
-          </button>
-        </div>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <div className="m-12">
+      <div className="bg-white p-8 rounded-lg shadow-lg">
+        <h1 className="text-3xl font-bold text-gray-800 mb-4">
+          Prueba de Tailwind
+        </h1>
+        <p className="text-gray-600 mb-4">
+          Tailwind funcionando correctamente.
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
