7c72a32ea5c0dc71adff16d2a8815c148e94ea98
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-08-06 20:10:25 -0300

    feat: adds Routes, Route and ProductDetailPage to App.tsx file

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import { Routes, Route } from "react-router-dom";
 import { ProductListPage } from "./pages/ProductListPage";
+import { ProductDetailPage } from "./pages/ProductDetailPage";
 
 function App() {
-  return <ProductListPage />;
+  return (
+    <Routes>
+      <Route path="/" element={<ProductListPage />} />
+      <Route path="/products/:productId" element={<ProductDetailPage />} />
+    </Routes>
+  );
 }
 
 export default App;
