f6bccc3edde20ed3fe14ea017c6b1dd29192b444
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-08-06 20:30:57 -0300

    feat: adds Footer to App component and improves its structure

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,13 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import { ProductListPage } from "./pages/ProductListPage";
 import { ProductDetailPage } from "./pages/ProductDetailPage";
+import { Navbar } from "./layouts/Navbar";
+import { Footer } from "./layouts/Footer";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<ProductListPage />} />
-      <Route path="/products/:productId" element={<ProductDetailPage />} />
-    </Routes>
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<ProductListPage />} />
+          <Route path="/products/:productId" element={<ProductDetailPage />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
