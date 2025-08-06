9316137c649492c018c3ebee2ea4b9672217fbb9
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-08-06 20:30:26 -0300

    feat: adds Footer component to layouts folder

---
+++ apps/frontend/src/layouts/Footer.tsx
@@ -0,0 +1,14 @@
+import { type FC } from "react";
+
+export const Footer: FC = () => {
+  return (
+    <footer className="bg-gray-800 text-white p-4 mt-auto">
+      <article className="container mx-auto text-center">
+        <p>
+          &copy; {new Date().getFullYear()} ForIT Tienda. Todos los derechos
+          reservados.
+        </p>
+      </article>
+    </footer>
+  );
+};
