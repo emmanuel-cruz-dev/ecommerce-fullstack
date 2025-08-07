225945f19d7bbaee8045709be400825bda9c92c3
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-08-07 12:15:36 -0300

    feat: adds authenticate and new route /me to auth.routes in backend folder

--- apps/backend/src/routes/auth.routes.ts
+++ apps/backend/src/routes/auth.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import authController from "../controllers/auth.controller";
+import { authenticate } from "../middlewares/auth.middleware";
 
 const router = Router();
 
 router.post("/register", authController.signUp);
 router.post("/login", authController.signIn);
+router.get("/me", authenticate, authController.getMe);
 
 export default router;
