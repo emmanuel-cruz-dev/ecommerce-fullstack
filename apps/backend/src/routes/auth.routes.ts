83a49a8d1406a3336160a86755d28bd0a4736acd
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-08-07 16:05:46 -0300

    feat: adds /refresh route and controller to auth.routes file

--- apps/backend/src/routes/auth.routes.ts
+++ apps/backend/src/routes/auth.routes.ts
@@ -7,5 +7,6 @@ const router = Router();
 router.post("/register", authController.signUp);
 router.post("/login", authController.signIn);
 router.get("/me", authenticate, authController.getMe);
+router.post("/refresh", authController.refresh);
 
 export default router;
