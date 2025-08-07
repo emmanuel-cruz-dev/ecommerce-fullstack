9f9643fc5301c724c6c23bd8cfc94af3d9a73113
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-08-07 12:14:14 -0300

    feat: adds LoginPage to pages folder

---
+++ apps/frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { useAuth } from "../context/auth.context";
+import { useNavigate } from "react-router-dom";
+
+export const LoginPage: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      await login({ email, password });
+      navigate("/");
+    } catch (error) {
+      alert(
+        `Credenciales incorrectas. Por favor, intenta de nuevo. Error: ${error}`
+      );
+    }
+  };
+
+  return (
+    <article className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-md">
+        <h2 className="text-3xl font-bold mb-6 text-center text-gray-800">
+          Iniciar Sesión
+        </h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Contraseña
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              required
+            />
+          </div>
+          <footer className="flex items-center justify-between">
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Entrar
+            </button>
+          </footer>
+        </form>
+      </div>
+    </article>
+  );
+};
