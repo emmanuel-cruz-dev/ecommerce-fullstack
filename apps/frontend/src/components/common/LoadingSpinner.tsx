6356ca97fddbe95998eeacfd1561dbb29844579d
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-08-07 16:33:36 -0300

    feat: adds LoadingSpinner component to components/common folder

---
+++ apps/frontend/src/components/common/LoadingSpinner.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const LoadingSpinner: React.FC = () => {
+  return (
+    <article
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <div
+        style={{
+          width: "50px",
+          height: "50px",
+          border: "5px solid #f3f3f3",
+          borderTop: "5px solid #3498db",
+          borderRadius: "50%",
+          animation: "spin 1s linear infinite",
+        }}
+      ></div>
+      <style>
+        {`
+          @keyframes spin {
+            0% { transform: rotate(0deg); }
+            100% { transform: rotate(360deg); }
+          }
+        `}
+      </style>
+    </article>
+  );
+};
+
+export default LoadingSpinner;
