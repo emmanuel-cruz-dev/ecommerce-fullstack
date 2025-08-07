10869513b05338fe7f81a261d70d242a7f82ce2b
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-08-07 16:07:19 -0300

    refactor: removes token from props in addToCart and getCart functions in frontend/src/services/cart.service.ts file

--- apps/frontend/src/services/cart.service.ts
+++ apps/frontend/src/services/cart.service.ts
@@ -1,16 +1,13 @@
 import api from "./api";
 import type { AddToCartRequest } from "../../../backend/src/types/types";
+import type { Cart } from "@domain/entities/Cart";
 
-export const addToCart = async (request: AddToCartRequest, token: string) => {
-  const response = await api.post(`/cart`, request, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
+export const addToCart = async (request: AddToCartRequest) => {
+  const response = await api.post(`/cart`, request);
   return response.data;
 };
 
-export const getCart = async (token: string) => {
-  const response = await api.get(`/cart`, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
+export const getCart = async (): Promise<Cart> => {
+  const response = await api.get(`/cart`);
   return response.data.payload;
 };
