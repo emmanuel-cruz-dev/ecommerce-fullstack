7480edf49042cd295e088b55c83099fcf7b44eb2
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-08-07 12:24:16 -0300

    feat: adds AuthGuard component to components/auth new folder

---
+++ apps/frontend/src/components/auth/AuthGuard.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "src/context/auth.context";
+
+export const AuthGuard: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const { isAuthenticated, isLoading } = useAuth();
+
+  if (isLoading) {
+    return (
+      <article className="flex items-center justify-center h-screen">
+        <p className="text-xl text-gray-500">Cargando...</p>
+      </article>
+    );
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+
+  return <>{children}</>;
+};
