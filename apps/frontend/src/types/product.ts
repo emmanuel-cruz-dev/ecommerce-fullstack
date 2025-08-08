c21e99408b8ed43faa903d9a991a1fd64ca7cb00
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-08-08 09:13:59 -0300

    refactor: changes ProductContainerProps location to product file in types folder

--- apps/frontend/src/types/product.ts
+++ apps/frontend/src/types/product.ts
@@ -3,3 +3,7 @@ import type { Product } from "@domain/entities/Product";
 export interface ProductCardProps {
   product: Product;
 }
+
+export interface ProductContainerProps {
+  products: Product[];
+}
