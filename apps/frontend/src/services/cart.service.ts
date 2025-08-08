56dfbf1290dc942eec9d66c3b014ea8c9378a184
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-08-08 07:56:47 -0300

    feat: adds clearCart function to cart.service file

--- apps/frontend/src/services/cart.service.ts
+++ apps/frontend/src/services/cart.service.ts
@@ -16,3 +16,7 @@ export const removeFromCart = async (productId: string): Promise<Cart> => {
   const response = await api.delete(`/cart/${productId}`);
   return response.data.payload;
 };
+
+export const clearCart = async (): Promise<void> => {
+  await api.delete("/cart");
+};
