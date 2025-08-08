5dd8a5b887e743a085d4053e273ea66bbaff44c4
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-08-08 08:26:45 -0300

    refactor: updates imports from useAuth in AuthGuard component

--- apps/frontend/src/components/auth/AuthGuard.tsx
+++ apps/frontend/src/components/auth/AuthGuard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-import { useAuth } from "src/context/auth.context";
+import { useAuth } from "src/hooks/useAuth";
 
 export const AuthGuard: React.FC<{ children: React.ReactNode }> = ({
   children,
