eb7e2ee6da6a923d71b0001eb7f22332a6244426
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-08-08 09:02:27 -0300

    refactor: changes const to function in ProductListPage

--- apps/frontend/src/pages/ProductListPage.tsx
+++ apps/frontend/src/pages/ProductListPage.tsx
@@ -1,10 +1,9 @@
-import { type FC } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getProducts } from "../services/product.service";
 import { ProductContainer } from "../components/ui/ProductContainer";
 import LoadingSpinner from "src/components/common/LoadingSpinner";
 
-export const ProductListPage: FC = () => {
+export function ProductListPage() {
   const {
     data: products,
     isLoading,
@@ -31,11 +30,11 @@ export const ProductListPage: FC = () => {
   }
 
   return (
-    <div className="container mx-auto p-4">
+    <article className="container mx-auto p-4">
       <h1 className="text-3xl font-bold text-gray-800 mb-6">
         Nuestros Productos
       </h1>
       <ProductContainer products={products || []} />
-    </div>
+    </article>
   );
-};
+}
